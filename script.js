a5dd7f7f6a3b8777f6aadfbc05a6452fb54c1e43
Papa Kofi Boahen <papakofiboahen@gmail.com>
2024-07-09 15:01:49 +0000

    string methods

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-const num1 = 8;
-const num2 = 6;
+const firstname = "Alan";
+const lastname = "Turing";
 
 let val;
-// division
-val = num1 / num2;
-// remainder
-val = num1 % num2;
 
-val = Math.PI;
-val = Math.E;
-val = Math.round(2.4);
-val = Math.ceil(2.4);
-val = Math.floor(2.8);
-val = Math.sqrt(64);
-val = Math.abs(-3);
-val = Math.pow(8, 2);
-val = Math.min(2, 33, 4, 1, 55, 6, 3, -2);
-val = Math.max(2, 33, 4, 1, 55, 6, 3, -2);
-val = Math.random();
-val = Math.floor(Math.random() * 5 + 1);
+// concatenate
+val = firstname + lastname;
+val = firstname + ' ' + lastname;
+
+// Append
+val = 'Alan ';
+val += 'Turing';
+
+// Escaping
+val = 'That\'s awesome, I can\'t wait';
+
+// Length
+val = firstname.length;
+
+// Concat
+val = firstname.concat(' ', lastname,);
+
 
 console.log(val);
