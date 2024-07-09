626ecc56cb2478630c0c6135dc9564eb983aa1ec
Papa Kofi Boahen <papakofiboahen@gmail.com>
2024-07-09 14:19:54 +0000

    variables

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-console.log('Hello, World!'); 
-console.log(123);
-console.log(true);
-var message = 'Hello, World!';
-console.log(message);
-console.log([1,2,3]);
-console.table({
-    name:'John',
-    age:30
-});
-console.error('This is some error');
-console.warn('This is a warning');
-console.clear();
-console.time('time');
-console.log('timing');
-console.log('timing');
-console.log('timing');
-console.log('timing');
-console.log('timing');
-console.log('timing');
-console.log('timing');
-console.timeEnd('time');
+var personName  = 'Alan Turing';
+
+console.log(personName);
+personName = 'John von Neumann';
+console.log(personName);
+
+var $name = 'Grace Hopper';
+console.log($name);
+
+var _name = 'Margaret Hamilton';
+console.log(_name);
+
+const user = {
+    name: 'Ada Lovelace',
+    age: 30
+    };
+user.name = 'Grace Hopper';
+
+console.log(user);
+
+const arr = [];
+console.log(arr);
