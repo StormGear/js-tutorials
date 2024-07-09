4b92b82da93825fdca9a55932ebf0de5f9c87d1d
Papa Kofi Boahen <papakofiboahen@gmail.com>
2024-07-09 14:13:10 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log('Hello, World!'); 
+console.log(123);
+console.log(true);
+var message = 'Hello, World!';
+console.log(message);
+console.log([1,2,3]);
+console.table({
+    name:'John',
+    age:30
+});
+console.error('This is some error');
+console.warn('This is a warning');
+console.clear();
+console.time('time');
+console.log('timing');
+console.log('timing');
+console.log('timing');
+console.log('timing');
+console.log('timing');
+console.log('timing');
+console.log('timing');
+console.timeEnd('time');
