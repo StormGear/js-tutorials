93330e338c42972dd9b4bb8e9781a65bd2cb2544
Papa Kofi Boahen <papakofiboahen@gmail.com>
2024-07-10 11:09:25 +0000

    functions

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-let val;
-let d = new Date('12-12-1999 12:00:00');
-d = new Date('December 12 1999');
-d = new Date('12/12/1999');
-// val = d;
+function user(name = 'Mary', age = 80) {
+    return 'Name: ' + name + ', Age: ' + age;
+}
 
-console.log(d);
+// console.log(user('Alan', 20)); 
 
-// Get the month
-val = d.getMonth();
-val = d.getDate(); 
-val = d.getDay();
-val = d.getFullYear();
-val = d.getHours();
-val = d.getMinutes();
-val = d.getTime(); 
-d.setMonth(2);
-d.setDate(12);
-d.setFullYear(1999);
-d.setHours(3);
-d.setMinutes(30);
-d.setSeconds(25);
-// console.log(d);
-// console.log(val);
+const sum = (a = 2, b = 3) => a + b;
+
+console.log(sum());
+
+// immediately invoked function expression
+(function() {
+    console.log('IIFE');
+})();
+
+const person = {
+    hello: 'John',
+    age: 30,
+    greet()  {
+        console.log('Hello there!');
+    }
+};
+
+person.greet();
 
