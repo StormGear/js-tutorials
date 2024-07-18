7c85e73a0ea7501812a00105777283e6ccc2772b
Papa Kofi Boahen <papakofiboahen@gmail.com>
2024-07-18 18:41:20 +0000

    ES6 classes

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
-  this.greet = function() {
-    return `Hello, my name is ${this.name} and I am ${this.age} years old`;
-  }
-  console.log(this.greet());
+const profilePrototype = {
+    message: function () {
+        return `Hello, I am ${this.firstName} ${this.lastName}`
+    },
+    setNewLastName (newLastName) {
+        this.lastName = newLastName
+    }
+
 }
 
-const person1 = new Person('John', 30);
-const person2 = new Person('Jane', 25);
+const peter = Object.create(profilePrototype)
+peter.firstName = 'Peter'
+peter.lastName = 'William'
+peter.age = 18
+peter.setNewLastName('Doe')
+console.log(peter.message())
 
-
-
+const alan = Object.create(profilePrototype, {
+    firstName: { value: 'Alan' },
+    lastName: { value: 'Smith' },
+    age: { value: 30 }
+})
+console.log(alan.message())
 
 
