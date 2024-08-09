4577165c6e32788c3cf5e55c9475967c3462a87f
Papa Kofi Boahen <papakofiboahen@gmail.com>
2024-08-09 11:26:16 +0000

    generators

--- app.js
+++ app.js
@@ -1,29 +1,79 @@
-let re;
-re = /hello/i;
-// console.log(re.source);
+function makeItemIterator(items) {
+    let nextIndex = 0;
+    const itemIterator = {
+        next:  () => {
+            let result = nextIndex < items.length ? {
+                value: items[nextIndex++],
+                done: false
+            } : {
+                    done: true
+                }
+            return result;
+        }
+    }
+    return itemIterator;
+}
+const itemsArr = ['Milk', 'Apple', 'Fish'];
+// const itemIterator = makeItemIterator(itemsArr);
+// console.log(itemIterator.next())
+// console.log(itemIterator.next())
+// console.log(itemIterator.next())
+// console.log(itemIterator.next())
+// console.log(itemIterator.next())
 
-//exec()
-// const result = re.exec('Hello everyone')
+// function* makeItemGenerator(items) {
+//     for (let i = 0; i < items.length; i++) {
+//         yield items[i];
+
+//     }
+// }
+// const itemGenerator = makeItemGenerator(itemsArr);
+// let result = itemGenerator.next()
 // console.log(result)
-// console.log(result[0])
-// console.log(result.index)
-// console.log(result.input)
+// while (!result.done) {
+//     console.log('generator result:', result)
+//     result = itemGenerator.next()
+// }
 
-//match()
-// const string = 'Hello, everyone, Hello';
-// const result = string.match(re)
-// console.log(result)
+// const myObject = {
+//     *[Symbol.iterator]() {
+//         yield 1;
+//         yield 2;
+//         yield 3;
+//     },
+//     greet() {
+//         console.log('Hello')
+//     }
+// }
 
-//test()
-// const result = re.test('Hello')
-// console.log(result)
+// for (const value of myObject) {
+//     console.log('value:', value)
+// }
 
-//search()
-// const string = 'Hi, Hello, Hello';
-// const result = string.search(re)
-// console.log(result)
+function* asyncGenerator() {
+    const result1 = yield fetch('https://jsonplaceholder.typicode.com/posts/1');
+    console.log('Result 1:', result1)
 
-//replace()
-const string = 'Hi, Hello, Hello';
-const result = string.replace(re, 'hey');
-console.log(result)
+    const result2 = yield fetch('https://jsonplaceholder.typicode.com/posts/2');
+    console.log('Result 2:', result2)
+}
+
+function run(generator) {
+    const iterator = generator();
+    const handle = (result) => {
+        if (result.done) return;
+        result.value.then(res => {
+            res.json().then(data => {
+                handle(iterator.next(data))
+            }
+        )
+        })
+    }
+    handle(iterator.next())
+}
+
+run(asyncGenerator)
+
+
+
+
