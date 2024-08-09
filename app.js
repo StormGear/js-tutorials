6abbee5f70aa09613f9cecc20d3b372b3c732273
Papa Kofi Boahen <papakofiboahen@gmail.com>
2024-08-09 14:45:47 +0000

    user with scroll

--- app.js
+++ app.js
@@ -1,53 +1,38 @@
-const users = [
-    {
-        "id": 7,
-        "email": "michael.lawson@reqres.in",
-        "first_name": "Michael",
-        "last_name": "Lawson",
-        "avatar": "https://reqres.in/img/faces/7-image.jpg"
-    },
-    {
-        "id": 8,
-        "email": "lindsay.ferguson@reqres.in",
-        "first_name": "Lindsay",
-        "last_name": "Ferguson",
-        "avatar": "https://reqres.in/img/faces/8-image.jpg"
-    },
-    {
-        "id": 9,
-        "email": "tobias.funke@reqres.in",
-        "first_name": "Tobias",
-        "last_name": "Funke",
-        "avatar": "https://reqres.in/img/faces/9-image.jpg"
-    },
-]
+//array destructuring
 
-document.getElementById('next').addEventListener('click', nextUser)
 
-function makeUserIterator(users) {
-    let nextIndex = 0;
-    const userIterator = {
-        next() {
-            let result;
-            result = nextIndex < users.length ? { value: users[nextIndex++], done: false } : { done: true }
-            return result;
-        }
-    }
-    return userIterator
+// [a, b, ...c] = [1, 2, 3, 4, 5, 6]
+// console.log(a);
+// console.log(c);
+// [user1, user2, user3] = ['Peter', 'Alan', 'John'];
+// console.log(user2);
+// function getItems() {
+//     return ['milk', 'fish', 'apple']
+// }
+// [item1, item2, item3] = getItems();
+// console.log(item2);
+//object destructuring
+// ({ b, a } = { a: "Mary", b: "Amy" });
+// console.log(a)
+// const user = {
+
+//     "id": 7,
+//     "email": "michael.lawson@reqres.in",
+//     "first_name": "Michael",
+//     "last_name": "Lawson",
+//     "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/follettkyle/128.jpg"
+
+// }
+
+// const id = user.id;
+// const { email, avatar } = user;
+// console.log(email)
+// console.log(avatar)
+
+function add() {
+    result = a + b;
+    return result;
 }
 
-const userIterator = makeUserIterator(users)
-
-nextUser();
-
-function nextUser() {
-    const user = userIterator.next().value
-    console.log(user)
-    if (user !== undefined) {
-        document.getElementsByClassName('card-title')[0].innerText = `${user.first_name} ${user.last_name}`;
-        document.querySelector('.card-text').innerText = `email: ${user.email}`;
-        document.querySelector('.card-img-top').src = user.avatar;
-    } else {
-        window.location.reload()
-    }
-}
+const socket = require('socket');
+console.log(here)
