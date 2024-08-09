cb0f63aa474de82fca9e452899f4907a3ecb2ea2
Papa Kofi Boahen <papakofiboahen@gmail.com>
2024-08-09 15:42:20 +0000

    Map data structure

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
-let uniqueKey = Symbol("key");
-const obj = {
-  [uniqueKey]: "value",
-  regularKey: "regularValue"
-};
-// uniqueKey = null;
-// console.log(obj[uniqueKey]); // "value"
-// console.log(uniqueKey.description); // "regularValue"
+const users = new  Map();
+const boys =   {
+    '1': 'John',
+    '2': 'Peter',
+    '3': 'Mary',
+}
 
-const person = {
-    name: "John",
-    [Symbol("id")]: 12345
-  };
+users.set('1', 'John');
+users.set('2', 'Peter');
+users.set('3', 'Mary');
+// users.set(function Peter() {}, {'Peter':'here'});
+// console.log(users.has('2')); // true
+// console.log(users.get('1')); // John
+// console.log(users.size); // 2
+// console.log(typeof boys); // object
+// console.log(users.delete('1')); // true
+// console.log(users.size); // 2
 
-console.log(person.Symbol); // undefined
+// for (const [key, value] of users) {
+//     console.log(key + ' = ' + value);
+// }
+
+for (const key of users.keys()) {
+    console.log(key);
+}
+
+for (const value of users.values()) {
+    console.log(value);
+}
+
+users.forEach((value, key) => {
+    console.log(key + ' = ' + value);
+});
+
+const usersArray = Array.from(users.keys()); 
+console.log(usersArray);
+
+const keysArray = Array.from(users.values()); 
+console.log(keysArray);
