c775996e1fa1dcae49afe43b06f47b26a24c9435
Papa Kofi Boahen <papakofiboahen@gmail.com>
2024-08-18 10:49:44 +0000

    Mediator Pattern

--- app.js
+++ app.js
@@ -1,55 +1,53 @@
-/* The `Click` class in JavaScript allows for subscribing, unsubscribing, and executing functions as
-observers. */
-class Click {
-    constructor() {
-    this.observers = [];
+class User {
+  constructor(name) {
+    this.name = name;
+    this.chatroom = null;
+  }
+
+    send(message, to) {
+        this.chatroom.send(message, this, to);
     }
 
-  /**
-   * The `subscribe` function adds a new function to the list of observers and logs a message
-   * indicating the subscription.
-   * @param func - The `func` parameter in the `subscribe` method is a function that you want to
-   * subscribe to. When you call the `subscribe` method and pass a function as an argument, that
-   * function will be added to the list of observers in the `observers` array.
-   */
-    subscribe(func) {
-        if (this.observers.includes(func)) {
-            console.log(`Already subscribed to this function: ${func.name}`);
-            return;
-        }
-        this.observers.push(func);
-        console.log(`Subscribed to this function: ${func.name}`);
-    }
-
-    unsubscribe(func) {
-        if (!this.observers.includes(func)) {
-            console.log(`Not subscribed to this function: ${func.name}`);
-            return;
-        }
-        this.observers = this.observers.filter(observer => observer !== func);
-        console.log(`Unsubscribed from this function: ${func.name}`);
-    }
-
-    execute() {
-        if (this.observers.length === 0) {
-            console.log('No observers to execute');
-            return;
-        }
-        this.observers.forEach(observer => observer());
+    receive(message, from) {
+        console.log(`${from.name} to ${this.name}: ${message}`);
     }
 }
 
-const click = new Click();
-document.querySelector('.sub1').addEventListener('click', () => click.subscribe(getCurrentTime));
-document.querySelector('.unsub1').addEventListener('click', () => click.unsubscribe(getCurrentTime));
-document.querySelector('.sub2').addEventListener('click', () => click.subscribe(getCurrentDate));
-document.querySelector('.unsub2').addEventListener('click', () => click.unsubscribe(getCurrentDate));
-document.querySelector('.exe').addEventListener('click', () => click.execute());
+class Chatroom {
+  constructor() {
+    this.users = {};
+  }
 
-function getCurrentTime() {
-    console.log(`Current time is ${new Date().toLocaleTimeString()}`);
+  register(user) {
+    this.users[user.name] = user;
+    user.chatroom = this;
+  }
+
+  send(message, from, to) {
+    if (to) {
+      // Single user message
+      to.receive(message, from);
+    } else {
+      // Mass message
+      for (key in this.users) {
+        if (this.users[key] !== from) {
+          this.users[key].receive(message, from);
+        }
+      }
+    }
+  }
 }
 
-function getCurrentDate() {
-    console.log(`Current date is ${new Date().toLocaleDateString()}`);
-}
+const Kofi = new User('Kofi');
+const Ama = new User('Ama');
+const Kwame = new User('Kwame');
+const chatroom = new Chatroom();
+console.log('Kofi before being registered in a chatroom', Kofi);
+// Register Kofi in the chatroom
+chatroom.register(Kofi);
+chatroom.register(Ama);
+chatroom.register(Kwame);
+console.log('Kofi after being registered in a chatroom', Kofi);
+Kofi.send('Hello Ama', Ama);
+
+
